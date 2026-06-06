5eb41f4a65ca760562f412847954f31c4d185ec1
John Rey <johnreyelep02@gmail.com>
2026-06-06 12:33:20 +0800

    Updated QuizPage

--- quiz-app/src/pages/QuizPage.jsx
+++ quiz-app/src/pages/QuizPage.jsx
@@ -25,20 +25,38 @@ export default function QuizPage() {
         loadQuiz();
     }, []);
 
-    if (!ready) return <h1>Loading...</h1>;
-    if (error) return <h1>Failed to load quiz. Please try again later.</h1>;
+    function ShowAnswer(correct_answer){
+        return (
+            <div>
+                <span>{correct_answer}</span>
+            </div>
+        );
+    }
 
     function QuestionCard() {
         return(
             <ul className='w-full grid grid-cols1 justify-center items-center gap-5'>
                 {quiz.map((q, index) => (
-                    <li key={index} className='w-150 h-50 border border-white rounded-2xl shadow-[0px_0px_5px_rgba(107,115,255,1)'>
-                        <div className='p-2 space-y-2'>
-                            <div className='flex justify-between'>
-                                <span>{q.category}</span>
-                                <span>{q.difficulty}</span>
+                    <li key={index} className='w-150 h-50 bg-white/10 backdrop-blur-2xl border border-white rounded-2xl shadow-[0px_0px_5px_rgba(107,115,255,1)'>
+                        <div className='relative flex flex-col p-2 space-y-3'>
+                            <div className='h-1/2 flex justify-between'>
+                                <span className='text-xl text-white font-semibold'>{q.category}</span>
+                                {
+                                    q.difficulty === "easy" && <span className='text-md text-green-500 font-semibold'>{q.difficulty}</span> ||
+                                    q.difficulty === "medium" && <span className='text-md text-blue-800 font-semibold'>{q.difficulty}</span>
+                                }
                             </div>
-                            <span>{q.question}</span>
+                            <span className='h-10 text-md text-white '>{q.question}</span>
+                            <div className=' '>
+                                <input type='text' placeholder='Enter your answer' className='w-full text-white border-b border-white focus:outline-none'/>
+                                <div className='w-full flex justify-between pt-5'>
+                                    <button className='text-white bg-blue-600/30 border border-white rounded-md py-2 px-3 cursor-pointer
+                                        hover:bg-blue-600'
+                                        onClick={ShowAnswer}>Show answer</button>
+                                    <button className='text-white bg-green-700/70 border border-white rounded-md py-2 px-3 cursor-pointer
+                                        hover:bg-green-700'>Submit answer</button>
+                                </div>
+                            </div>
                         </div>
                     </li>
                 ))}
@@ -46,8 +64,18 @@ export default function QuizPage() {
         );
     }
     return (
-        <>
+        <div className='relative w-full flex flex-col justify-center items-center pt-20'>
+            <h1 className='text-6xl text-white font-semibold pb-10'>QuizGame.io</h1>
+            {
+                !ready && <div className='absolute'>
+                    <span className='loading'>Loading...</span>
+                </div>
+            }
+            {
+                error && <div></div>
+            }
+
             <QuestionCard/>
-        </>
+        </div>
     );
 }
