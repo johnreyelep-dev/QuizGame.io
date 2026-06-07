403cb601103850fc3fa259993ae8a0db1dac0566
John Rey <johnreyelep02@gmail.com>
2026-06-07 22:16:36 +0800

    Updated QuizPage

--- quiz-app/src/pages/QuizPage.jsx
+++ quiz-app/src/pages/QuizPage.jsx
@@ -1,13 +1,25 @@
 import { useState} from "react";
 import { useEffect } from "react";
 import { quizApi } from "../services/quizApi.jsx";
+import { Link } from "react-router-dom";
 
-function PopUp({index, result}){
-    return <div>
+function PopUp({index, result, setResult}){
+    return (
+        <div className={`relative flex w-full h-full justify-center items-center rounded-2xl pointer-events-auto transition-all duration-300 ease-in-out 
+            ${result[index] === true ? "bg-green-600" : "bg-red-600"}`}>
         {
-            result[index] === true ? <span>Correct</span> : <span>Wrong</span>
+            result[index] === true ? <span className='text-3xl text-white font-semibold'>Correct</span> : <span className='text-3xl text-white font-semibold'>Wrong</span>
         }
+        <div className='absolute group bg-white/40 w-6 h-6 flex justify-center items-center top-2 right-3 rounded-full pb-2 transition-all duration-300 ease-in hover:bg-white'>
+            <button className={`text-2xl text-white font-bold cursor-pointer ${result[index] === true ? "group-hover:text-green-500" : "group-hover:text-red-600"}`}
+            onClick={() => setResult(prev => {
+                const next = {...prev};
+                delete next[index];
+                return next;
+            })}>x</button>
+        </div>
     </div>
+    );
 }
 
 function QuestionCard({quiz}) {
@@ -27,19 +39,21 @@ function QuestionCard({quiz}) {
     }
 
     return(
-        <ul className='w-full grid grid-cols1 justify-center items-center gap-5'>
+        <ul className='w-full h-[60vh] flex flex-col items-center gap-5 overflow-y-scroll mb-5'>
             {quiz.map((q, index) => (
                 <li key={index} className='w-150 h-50 bg-white/10 backdrop-blur-2xl border border-white rounded-2xl shadow-[0px_0px_5px_rgba(107,115,255,1)'>
-                    <div className='relative flex flex-col p-2 space-y-3'>
+                    <div className='relative w-full flex flex-col p-2 space-y-3'>
                         <div className='h-1/2 flex justify-between'>
                             <span className='text-xl text-white font-semibold'>{q.category}</span>
                             {
-                                q.difficulty === "easy" && <span className='text-md text-green-500 font-semibold'>{q.difficulty}</span> ||
+                                q.difficulty === "easy" && <span className='text-md text-green-500 font-semibold'>{q.difficulty}</span>
+                            }
+                            {
                                 q.difficulty === "medium" && <span className='text-md text-blue-800 font-semibold'>{q.difficulty}</span>
                             }
                         </div>
                         <span className='h-10 text-md text-white '>{q.question}</span>
-                        <div className=' '>
+                        <div className=''>
                             <input type='text'
                                    placeholder='Enter your answer'
                                    value={answer[index] || ""}
@@ -50,19 +64,21 @@ function QuestionCard({quiz}) {
                                         hover:bg-blue-600'
                                         onClick={() => setRevealed((prev) => ({...prev, [index]: !prev[index]}))}>Show answer</button>
                                 {
-                                    revealed[index] && <span className=''>{q.correct_answer}</span>
+                                    revealed[index] && <span className='text-xl text-white font-bold'>Answer: {q.correct_answer}</span>
                                 }
                                 <button className='text-white bg-green-700/70 border border-white rounded-md py-2 px-3 cursor-pointer
                                         hover:bg-green-700'
                                         onClick={() => handleSubmit(index, q.correct_answer)}>Submit answer</button>
                             </div>
                         </div>
-                        {
-                            result[index] !== undefined &&  <PopUp result={result} index={index}/>
+
+                        { result[index] !== undefined && (
+                            <div className='absolute w-full h-[70%] flex justify-center items-center pointer-events-none'>
+                                <div className='flex w-[70%] h-[80%] justify-center items-center'>
+                                      <PopUp result={result} index={index} setResult={setResult}/>
+                                </div>
+                            </div>)
                         }
-                        {/*{*/}
-                        {/*    result[index] === false && <span>Wrong answer</span>*/}
-                        {/*}*/}
                     </div>
                 </li>
             ))}
@@ -108,6 +124,8 @@ export default function QuizPage() {
             {
                 ready && !error && <QuestionCard quiz={quiz}/>
             }
+            <Link to='/' className='bg-white/10 w-150 h-15 flex justify-center items-center text-3xl text-white font-semibold rounded-2xl border border-white
+            transition-all duration-300 ease-in hover:shadow-[0px_0px_5px_5px_rgba(107,115,255,0.7)] hover:-translate-y-2'>Back</Link>
         </div>
     );
 }
